a41ff44db826eedfe270be486f786e125fe0e2eb
Haolun <chshawkn@users.noreply.github.com>
2017-08-11 15:16:21 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# oss-github
+Infrastructure specific CI configurations for projects on github and depends on public infrastructure
+
+## Directory structure
+
+- src/main/ci-script
+> ci_opts.sh for oss-build's CI script
+
+- src/main/maven
+> maven settings.xml
+
+## See also
+
+[oss-internal-template](https://github.com/home1-oss/oss-internal-template)
+[oss-local](https://github.com/home1-oss/oss-local)
--- src/main/ci-script/ci_opts.sh
+++ src/main/ci-script/ci_opts.sh
@@ -1,5 +1,5 @@
 
-#export MAVEN_OPTS="${MAVEN_OPTS} -Dcheckstyle.config.location=${BUILD_SCRIPT_LOC}/src/main/checkstyle/google_checks_6.19.xml"
-#export MAVEN_OPTS="${MAVEN_OPTS} -Dpmd.ruleset.location=${BUILD_SCRIPT_LOC}/src/main/pmd/pmd-ruleset-5.3.5.xml"
+#export MAVEN_OPTS="${MAVEN_OPTS} -Dcheckstyle.config.location=https://github.com/home1-oss/oss-build/raw/master/src/main/checkstyle/google_checks_6.19.xml"
+#export MAVEN_OPTS="${MAVEN_OPTS} -Dpmd.ruleset.location=https://github.com/home1-oss/oss-build/raw/master/src/main/pmd/pmd-ruleset-5.3.5.xml"
 #export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.nodeDownloadRoot=https://nodejs.org/dist/"
 #export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.npmDownloadRoot=https://registry.npmjs.org/npm/-/"
